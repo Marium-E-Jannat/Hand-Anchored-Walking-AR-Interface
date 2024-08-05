0e2d7877d483b57ed5e31e8aebc9dced345c6993
Thuan Vo <thuan.votann@gmail.com>
2024-08-05 04:23:20 -0700

    fix hand pinching detect

--- Assets/scripts/fittsrecorder2.cs
+++ Assets/scripts/fittsrecorder2.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 using UnityEngine.UI;
 using Firebase;
 using Firebase.Database;
@@ -15,7 +14,6 @@ public class fittsrecorder2 : MonoBehaviour
     public Color colorAtStill;
     public static int selec;
     public static float startTime;
-    [SerializeField] private Button startButton;
     [SerializeField] private OVRHand rightHand;
     public static bool quizInProgress = false;
     int quizNumber = 0;
@@ -23,12 +21,14 @@ public class fittsrecorder2 : MonoBehaviour
     int iteration = 0;
     public UnityEvent startNewQuiz = new UnityEvent();
     public GameObject cursor;
+
     enum status {
         ERROR,
         SUCCESS,
         OUTLIER,
         UNPINCH
     }
+    bool wasPinching = false;
     public float distanceUpperThreshold = 0.2f, distanceLowerThreshold = 0.05f;
     private List<Dictionary<string, object>> responses = new List<Dictionary<string, object>>();
 
@@ -51,61 +51,56 @@ public class fittsrecorder2 : MonoBehaviour
                 optionNumber = (quizNumber + 4) % 8;
             }
             ShowQuestion();
-            status statusCode = CheckForPinch();
-            if(statusCode == status.UNPINCH){
-                return;
-            }
-            if(statusCode != status.OUTLIER) {
-                if(statusCode == status.ERROR){
-                    Debug.Log("Error detected, iteration counted.");
-                }else{
-                    Debug.Log("Success, iteration counted.");
-                }
-                ResetButtonColor();
-                iteration += 1;
-                if (iteration == 10){
-                    quizInProgress = false;
-                    iteration = 0;
-                    quizNumber += 1;
-                    if (quizNumber < 8){
-                        startNewQuiz.Invoke();
-                    }else {
-                        SaveResponsesToFirebase();
-                        Debug.Log("Quiz complete!");
-                        return;
+            if(wasPinching && !rightHand.GetFingerIsPinching(OVRHand.HandFinger.Index)) {
+                status newStatusCode = CheckForPinch();
+
+                if (newStatusCode != status.OUTLIER)
+                {
+                    if (newStatusCode == status.ERROR)
+                    {
+                        Debug.Log("Error detected, iteration counted.");
+                    }
+                    else
+                    {
+                        Debug.Log("Success, iteration counted.");
+                    }
+                    ResetButtonColor();
+                    iteration += 1;
+                    if (iteration == 10)
+                    {
+                        quizInProgress = false;
+                        iteration = 0;
+                        quizNumber += 1;
+                        if (quizNumber < 8)
+                        {
+                            startNewQuiz.Invoke();
+                        }
+                        else
+                        {
+                            SaveResponsesToFirebase();
+                            Debug.Log("Quiz complete!");
+                            return;
+                        }
                     }
                 }
-            }else{
-                Debug.Log("Outlier detected, iteration not counted.");
+                else
+                {
+                    Debug.Log("Outlier detected, iteration not counted.");
+                }
+
+                Debug.Log("Option Selected: " + selec);
+                HandleOptionSelected(newStatusCode);
+         
             }
-            Debug.Log("Option Selected: " + selec);
-            HandleOptionSelected(statusCode);
         }
+        wasPinching = rightHand.GetFingerIsPinching(OVRHand.HandFinger.Index);
         // reset iteration, status, metrics
-    }
-
-    private void hello()
-    {
-        Image image = startButton.GetComponent<Image>();
-        image.color = colorAtStill;
-
-        // TODO: rewrite to reset only selected button
-        for (int i = 0; i < optionButtons.Length; i++)
-        {
-            Image images = optionButtons[i].GetComponent<Image>();
-            images.color = colorAtStill;
-            TextMeshProUGUI texts = optionButtons[i].GetComponentInChildren<TextMeshProUGUI>();
-            texts.color = Color.black;
-        }
-
-        TextMeshProUGUI text = startButton.GetComponentInChildren<TextMeshProUGUI>();
-        text.color = Color.black;
     }
 
     status CheckForPinch()
     {
-        if (rightHand != null && rightHand.GetFingerIsPinching(OVRHand.HandFinger.Index))
-        {
+        //if (rightHand != null && rightHand.GetFingerIsPinching(OVRHand.HandFinger.Index))
+        //{
 
             Vector3 pinchPosition = cursor.transform.position;
 
@@ -128,70 +123,11 @@ public class fittsrecorder2 : MonoBehaviour
             }else{
                 return status.SUCCESS;
             }
-        }else{
-            return status.UNPINCH;
-        }
+        //}
+        //else{
+        //    return status.UNPINCH;
+        //}
     }
-
-    // private IEnumerator InitializeQuiz()
-    // {
-    //     for (qindex = 0; qindex < 8; qindex++)
-    //     {
-    //         currentQuestionIndex = qindex;
-    //         cindex=-1;
-    //         yield return StartCoroutine(hello());
-    //         // Wait for the right hand pinching to reset
-    //         yield return new WaitUntil(()=>rightHand.GetFingerIsPinching(OVRHand.HandFinger.Index) == false);
-
-    //         Image image = startButton.GetComponent<Image>();
-    //         image.color = colorAtStill;
-    //         TextMeshProUGUI text = startButton.GetComponentInChildren<TextMeshProUGUI>();
-    //         text.color = Color.black;
-
-    //         int first = qindex;
-    //         int second = (qindex + 4) % 8;
-    //         int send = first, t = 0;
-
-    //         for (int i = 0; i < 10; i++)
-    //         {
-
-    //             Debug.Log("iteration is " + i);
-    //             Debug.Log("send before question shown is " + send);
-    //             ShowQuestion(send);
-    //             selec = -1;
-
-    //             yield return new WaitUntil(() =>
-    //             {
-    //                 CheckForPinch(send);
-    //                 return selec != -1;
-    //             });
-
-    //             if (selec == -3)
-    //             {
-    //                 Debug.Log("Outlier detected, iteration not counted.");
-    //                 i--;
-    //                 selec = -1;
-    //                 continue;
-    //             }
-    //             else if (selec == -2)
-    //             {
-    //                 Debug.Log("Error detected, iteration counted.");
-    //                 selec = -1;
-    //                 t ^= 1;
-    //                 send = (t == 1) ? second : first;
-    //                 continue;
-    //             }
-
-    //             Debug.Log("Option Selected: " + selec);
-    //             yield return StartCoroutine(HandleOptionSelected(selec - 1));
-
-    //             t ^= 1;
-    //             send = (t == 1) ? second : first;
-    //         }
-    //     }
-    //     SaveResponsesToFirebase();
-    //     Debug.Log("Quiz complete!");
-    // }
 
     public void ShowQuestion()
     {
