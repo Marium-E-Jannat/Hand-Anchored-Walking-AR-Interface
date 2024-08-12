8014aae945783f66179c3d69ce1ab94bff1d9b2d
nganphan123 <jill01009@gmail.com>
2024-08-12 16:55:59 -0700

    fix far pinch

--- Assets/scripts/ClosePinch.cs
+++ Assets/scripts/ClosePinch.cs
@@ -3,22 +3,14 @@ using UnityEngine;
 public class ClosePinch : MonoBehaviour
 {
     [SerializeField] private OVRHand handUsedForPinch; // The hand used for pinch detection
-    [SerializeField] private Transform canvasTransform; // The transform of the canvas
-    [SerializeField] private float moveSpeed = 1.0f; // Speed of the canvas movement
-    [SerializeField] private float rotationSpeed = 50.0f; // Speed of the canvas rotation
     [SerializeField] private bool mockPinch = false; // Mock pinch for testing without hardware
     [SerializeField] private GameObject indexFingerPad;
-    [SerializeField] private GameObject centerHead;
     // private bool isPinching;
-    [SerializeField] private bool closeDistance;
+    [SerializeField] private bool farDistance;
     bool wasPinching = false;
     private Transform headTransform;
-    Vector3 canvasHalfSize;
 
     void Start(){
-        RectTransform rectTransform = transform.GetComponent<RectTransform>();
-        Vector2 size = rectTransform.sizeDelta * rectTransform.localScale;
-        canvasHalfSize = new Vector3(size.x / 2.0f, size.y / 2.0f, 0);
         // Locate the main camera tagged as "MainCamera"
         headTransform = Camera.main != null ? Camera.main.transform : null;
 
@@ -37,6 +29,9 @@ public class ClosePinch : MonoBehaviour
             if(!wasPinching){
                 transform.position = indexFingerPad.transform.position;
                 transform.rotation = Quaternion.LookRotation(transform.position - headTransform.position);
+                if(farDistance){
+                    transform.position += transform.forward.normalized;
+                }
             }
         }
         wasPinching = IsFourFingerPinching();
