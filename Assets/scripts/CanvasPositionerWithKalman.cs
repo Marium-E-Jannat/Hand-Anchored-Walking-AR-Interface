8ce2a6511b41e7d7c55a19b376d9b6ce81ab7729
nganphan123 <jill01009@gmail.com>
2024-08-13 12:13:27 -0700

    add kalman filter and circular buffar

---
+++ Assets/scripts/CanvasPositionerWithKalman.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class CanvasPositionerWithKalman : MonoBehaviour
+{
+    protected Vector3 originPoint, originRotationVector;
+    KalmanFilterVector3 kalmanV3Origin, kalmanV3Rotation;
+    CircularBuffer.CircularBuffer<Vector3> originHistory, rotationHistory;
+    [Header("Kalman Filters")]
+    [SerializeField] private int originHistoryWindow = 4;
+    [SerializeField] private int rotationHistoryWindow = 6;
+    [SerializeField] private float k1q = 0.0001f, k2q = 0.0001f, k1r = 0.1f, k2r = 0.1f;
+    [SerializeField] private float k1qEyes = 0.0001f, k2qEyes = 0.00001f, k1rEyes = 0.1f, k2rEyes = 0.000001f;
+    [SerializeField] private GameObject controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetKalmanFilters();
+        originPoint = Vector3.zero;
+        originRotationVector = Vector3.zero;
+    }
+
+    public void ResetKalmanFilters()
+    {
+        kalmanV3Origin = new KalmanFilterVector3(k1q, k1r);
+        kalmanV3Rotation = new KalmanFilterVector3(k2q, k2r);
+
+        originHistory = new CircularBuffer.CircularBuffer<Vector3>(originHistoryWindow);
+        rotationHistory = new CircularBuffer.CircularBuffer<Vector3>(rotationHistoryWindow);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.SetPositionAndRotation(GetStableCanvasPosition(), Quaternion.Euler(GetStableCanvasOrientation()));
+    }
+
+    private Vector3 GetStableCanvasPosition()
+    {
+        Transform tf = controller.transform;
+        if (tf != null)
+        {
+            originPoint = tf.position;
+            originHistory.PushBack(originPoint);
+            originPoint = kalmanV3Origin.Update(originHistory.Back(), k1q, k1r);
+        }
+        return originPoint;
+    }
+
+    private Vector3 GetStableCanvasOrientation()
+    {
+        Transform tf = controller.transform;
+        if (tf != null)
+        {
+            originRotationVector = tf.rotation.eulerAngles;
+            rotationHistory.PushBack(originRotationVector);
+            originRotationVector = kalmanV3Rotation.Update(rotationHistory.Back(), k2q, k2r);
+        }
+        return originRotationVector;
+    }
+}
---
+++ Assets/scripts/CanvasPositionerWithThreshold.cs
@@ -0,0 +1,110 @@
+using Firebase;
+using Firebase.Database;
+using Firebase.Extensions;
+using UnityEngine;
+
+public class CanvasPositionerWithThreshold : MonoBehaviour
+{
+    [SerializeField] private GameObject controller;
+
+    [SerializeField] private float positionThreshold;
+
+    [SerializeField] private float rotationThreshold;
+    [SerializeField] private float speed;
+
+    private DatabaseReference databaseReference;
+    private Vector3 newPosition;
+
+    void Start(){
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+        {
+            databaseReference = FirebaseDatabase.DefaultInstance.RootReference;
+            ListenForPosThresChange();
+            ListForRotThres();
+            ListenForSpeedChange();
+            newPosition = transform.position;
+        });
+    }
+
+    void ListForRotThres(){
+        databaseReference.Child("Rotation-threshold").ValueChanged += (object sender, ValueChangedEventArgs args) => { 
+            if (args.DatabaseError != null)
+            {
+                Debug.LogError(args.DatabaseError.Message);
+                return;
+            }
+            if (args.Snapshot.Exists)
+            {
+                Debug.Log("rotation threshold from firebase " + args.Snapshot.Value.ToString());
+                if(float.TryParse(args.Snapshot.Value.ToString(), out float result))
+                {
+                    rotationThreshold = result;
+                }
+            }
+        };
+    }
+
+    void ListenForSpeedChange(){
+        databaseReference.Child("Speed").ValueChanged += (object sender, ValueChangedEventArgs args) => { 
+            if (args.DatabaseError != null)
+            {
+                Debug.LogError(args.DatabaseError.Message);
+                return;
+            }
+            if (args.Snapshot.Exists)
+            {
+                Debug.Log("speed from firebase " + args.Snapshot.Value.ToString());
+                if(float.TryParse(args.Snapshot.Value.ToString(), out float result))
+                {
+                    speed = result;
+                }
+            }
+        };
+    }
+
+    void ListenForPosThresChange(){
+        databaseReference.Child("Position-threshold").ValueChanged += (object sender, ValueChangedEventArgs args) => { 
+            if (args.DatabaseError != null)
+            {
+                Debug.LogError(args.DatabaseError.Message);
+                return;
+            }
+            if (args.Snapshot.Exists)
+            {
+                Debug.Log("position threshold from firebase " + args.Snapshot.Value.ToString());
+                if(float.TryParse(args.Snapshot.Value.ToString(), out float result))
+                {
+                    positionThreshold = result;
+                }
+            }
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTransformWithController();
+    }
+
+    void UpdateTransformWithController() {
+        if (controller == null) {
+            Debug.Log("No controller was set");
+            return;
+        }
+
+        Transform ctf = controller.transform;
+
+        if (Vector3.Distance(ctf.position, newPosition) > positionThreshold) {
+            newPosition = ctf.position;
+        }
+
+        var step = speed * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, newPosition, step);
+
+        Quaternion tRot = Quaternion.LookRotation(ctf.position - transform.position);
+        float angleDifference = Quaternion.Angle(transform.rotation, tRot);
+        if (angleDifference > rotationThreshold) {
+            transform.rotation = ctf.rotation;
+        }
+    }
+}
