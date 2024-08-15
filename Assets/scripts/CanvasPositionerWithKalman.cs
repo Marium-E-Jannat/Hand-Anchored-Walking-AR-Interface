907a50e270daf25c7344e839d88bf4ec78c9742f
nganphan123 <jill01009@gmail.com>
2024-08-14 21:04:48 -0700

    fix kalman rotation

--- Assets/scripts/CanvasPositionerWithKalman.cs
+++ Assets/scripts/CanvasPositionerWithKalman.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using Firebase.Database;
 using UnityEngine;
 
 public class CanvasPositionerWithKalman : MonoBehaviour
@@ -6,11 +9,13 @@ public class CanvasPositionerWithKalman : MonoBehaviour
     KalmanFilterVector3 kalmanV3Origin, kalmanV3Rotation;
     CircularBuffer.CircularBuffer<Vector3> originHistory, rotationHistory;
     [Header("Kalman Filters")]
-    [SerializeField] private int originHistoryWindow = 4;
-    [SerializeField] private int rotationHistoryWindow = 6;
-    [SerializeField] private float k1q = 0.0001f, k2q = 0.0001f, k1r = 0.1f, k2r = 0.1f;
-    [SerializeField] private float k1qEyes = 0.0001f, k2qEyes = 0.00001f, k1rEyes = 0.1f, k2rEyes = 0.000001f;
+    [SerializeField] private int originHistoryWindow = 6;
+    [SerializeField] private int rotationHistoryWindow = 10;
+    [SerializeField] private float k1q = 0.0001f, k2q = 0.00001f, k1r = 0.1f, k2r = 0.01f;
+    // [SerializeField] private float k1qEyes = 0.0001f, k2qEyes = 0.00001f, k1rEyes = 0.1f, k2rEyes = 0.000001f;
     [SerializeField] private GameObject controller;
+    private DatabaseReference databaseReference;
+    //bool dirxold = false;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +23,31 @@ public class CanvasPositionerWithKalman : MonoBehaviour
         ResetKalmanFilters();
         originPoint = Vector3.zero;
         originRotationVector = Vector3.zero;
+        List<string> datafields = new List<string>{"k1q","k1r","k2q","k2r","originWindow","rotationWindow"};
+        List<Action<float>> callbacks = new List<Action<float>> { 
+            (float newVal) => {k1q = newVal;},
+            (float newVal) => {k1r = newVal;},
+            (float newVal) => {k2q = newVal;},
+            (float newVal) => {k2r = newVal;},
+            (float newVal) => {originHistoryWindow = (int) newVal;},
+            (float newVal) => {rotationHistoryWindow = (int) newVal;},
+        };
+        new FirebaseTracking(datafields, callbacks);
+        k1q = (float)databaseReference.Child("k1q").GetValueAsync().Result.Value;
+        k1r = (float)databaseReference.Child("k1r").GetValueAsync().Result.Value;
+        k2q = (float)databaseReference.Child("k2q").GetValueAsync().Result.Value;
+        k2r = (float)databaseReference.Child("k2r").GetValueAsync().Result.Value;
+        //originHistoryWindow = (int)databaseReference.Child("originWindow").GetValueAsync().Result.Value;
+        //rotationHistoryWindow = (int)databaseReference.Child("rotationWindow").GetValueAsync().Result.Value;
     }
 
     public void ResetKalmanFilters()
     {
         kalmanV3Origin = new KalmanFilterVector3(k1q, k1r);
         kalmanV3Rotation = new KalmanFilterVector3(k2q, k2r);
+        // kalmanV3Origin.Reset();
+        // kalmanV3Rotation.Reset();
+        // kalmanV3Rotation.x = transform.rotation.eulerAngles;
 
         originHistory = new CircularBuffer.CircularBuffer<Vector3>(originHistoryWindow);
         rotationHistory = new CircularBuffer.CircularBuffer<Vector3>(rotationHistoryWindow);
@@ -32,7 +56,8 @@ public class CanvasPositionerWithKalman : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.SetPositionAndRotation(GetStableCanvasPosition(), controller.transform.rotation);
+        // transform.SetPositionAndRotation(GetStableCanvasPosition(), controller.transform.rotation);
+        transform.SetPositionAndRotation(GetStableCanvasPosition(), Quaternion.Euler(GetStableCanvasOrientation()));
     }
 
     private Vector3 GetStableCanvasPosition()
@@ -50,11 +75,28 @@ public class CanvasPositionerWithKalman : MonoBehaviour
     private Vector3 GetStableCanvasOrientation()
     {
         Transform tf = controller.transform;
+        //bool dirx = false;
+        //bool diry = false;
+        //bool dirz = false;
         if (tf != null)
         {
             originRotationVector = tf.rotation.eulerAngles;
+            originRotationVector = new Vector3((originRotationVector.x + 180) % 360, (originRotationVector.y + 180) % 360, originRotationVector.z);
+            // if(originRotationVector.x > 180) {
+            //     dirx = true;
+            //     originRotationVector = new Vector3(360 - originRotationVector.x, originRotationVector.y, originRotationVector.z);
+            // }
+
+            // if(originRotationVector.x > 5 && originRotationVector.x < 175) {
+            //     dirxold = dirx;
+            // }
+            Debug.Log("origin rotation vector 1" + originRotationVector);
             rotationHistory.PushBack(originRotationVector);
+
             originRotationVector = kalmanV3Rotation.Update(rotationHistory.Back(), k2q, k2r);
+            originRotationVector = new Vector3((originRotationVector.x - 180) % 360, (originRotationVector.y - 180) % 360, originRotationVector.z);
+            // Debug.Log("origin rotation vector 2" + originRotationVector);
+            // if(dirxold) originRotationVector = new Vector3(360 - originRotationVector.x, originRotationVector.y, originRotationVector.z);
         }
         return originRotationVector;
     }
