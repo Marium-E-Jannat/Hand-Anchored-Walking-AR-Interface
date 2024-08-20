442842f768167fe334d2c8e02920e53a69e04882
nganphan123 <jill01009@gmail.com>
2024-08-20 01:54:21 -0700

    add chest tracker for canvas movement

--- Assets/scripts/screen corrector.cs
+++ Assets/scripts/screen corrector.cs
@@ -3,13 +3,18 @@ using UnityEngine.Serialization;
 
 public class FollowInZDirection : MonoBehaviour
 {
-    public Transform target;      // The object to follow
-    [FormerlySerializedAs("followSpeed")] public float offset = 5f; // Speed at which the follower moves
+    public Transform chest;      // The object to follow
+    [FormerlySerializedAs("followSpeed")] public float offset; // Speed at which the follower moves
 
-    void LateUpdate()
+    void Update()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y, target.position.z) + new Vector3(0,0,offset);
+        // heighten canvas to the head
+        // transform.position = new Vector3(transform.position.x, head.position.y, -transform.position.z);
+        // transform.rotation = chest.rotation;
+        // // rotate the canvas to face the user
+        // transform.Rotate(new Vector3 (0,0,90));
+        // transform.rotation = Quaternion.LookRotation(transform.position - chest.position);
+        transform.SetPositionAndRotation(chest.transform.position + chest.transform.up.normalized * offset, Quaternion.LookRotation(transform.position - chest.position));
+        // transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + offset);
     }
-    
-
 }
